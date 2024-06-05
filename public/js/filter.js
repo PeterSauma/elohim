30465fd46671d095d15058a348b06a91e0dd8edb
Peter Sauma <petersauma@gmail.com>
2024-06-05 16:32:58 -0300

    se modifica el filter de la pagina todos los productos

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,261 +1,72 @@
-window.addEventListener('load', function(){
+document.addEventListener("DOMContentLoaded", function() {
+    const checkboxes = document.querySelectorAll('.form-check-input.brand');
+    const priceRadios = document.querySelectorAll('.form-check-input.price');
+    const products = document.querySelectorAll('.grid');
 
-    const btn = document.querySelector(".btn-filter")
-    const filterMenu = document.querySelector(".filter-menu")
-    const arrowLeft = document.querySelector('.arrow-left')
+    // Function to filter products based on checked checkboxes and price
+    function filterProducts() {
+        const checkedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked);
+        const checkedPrices = Array.from(priceRadios).filter(radio => radio.checked);
 
-    // filter menu - click on button
-    btn.addEventListener("click", () => {
-        filterMenu.classList.add('display-block')
-        filterMenu.classList.toggle('active')
-    })
+        products.forEach(product => {
+            const productCategory = product.classList.contains('ropa') ? 'ropa' :
+                                    product.classList.contains('detergentes') ? 'detergentes' :
+                                    product.classList.contains('pisos') ? 'pisos' :
+                                    product.classList.contains('cosmetica') ? 'cosmetica' :
+                                    product.classList.contains('autos') ? 'autos' : '';
+            
+            const productBrand = product.querySelector('.brand-product h5').textContent.trim();
+            const productPrice = parseFloat(product.dataset.price);
 
-    arrowLeft.addEventListener('click', () => {
-        filterMenu.classList.remove('display-block')
-        filterMenu.classList.toggle('active')
-    })
+            let categoryMatch = true;
+            let brandMatch = true;
+            let priceMatch = true;
 
-    
-    // Function Filter
-
-    var checkbox = document.querySelectorAll(".form-check-input.brand");
-    var checkboxPrice = document.querySelectorAll(".form-check-input.price");
-    var checkboxDiscount = document.querySelectorAll(".form-check-input.discount");
-    var cards = document.querySelectorAll(".cards")
-    let cardsFilterByBrand = []
-        
-    let arrayFilter, arrayNoFilter
-    
-    // ver que funcione y reducir el array sin repeticiones
-
-// capturar el evento de captura cambios en los checkbox de brand
-    for(i = 0; i < checkbox.length; i++) {
-
-        checkbox[i].addEventListener('change', function() {
-            let arr1 = []
-            let arr2 = [] 
-            for(i = 0; i < checkbox.length; i++) {
-            if (checkbox[i].checked) { 
-                let combineValueCategory = checkbox[i].value + "-" + checkbox[i].dataset.category
-               
-                arr1.push(combineValueCategory)
-                arrayFilter = Array.from(new Set(arr1))            
-            } else {
-                let combineValueCategoryNo = checkbox[i].value + "-" + checkbox[i].dataset.category
-                arr2.push(combineValueCategoryNo)
-                arrayNoFilter = Array.from(new Set(arr2))
-                // el 9 corresponde al numero de filtros seteados. De agregar o reducir la cantidad, se debe modificar este numero
-                if (arrayNoFilter.length == 9) {
-                    arrayFilter = []
-                }   
+            if (checkedCheckboxes.length > 0) {
+                brandMatch = checkedCheckboxes.some(checkbox => checkbox.value === productBrand);
+                categoryMatch = checkedCheckboxes.some(checkbox => checkbox.dataset.category === productCategory);
             }
-            } 
-            console.log(arrayFilter)
-            cardsFilterByBrand = []
-            filterBrand()           
-            for(i = 0; i < checkboxPrice.length; i++) {
-                if (checkboxPrice[i].checked) {
-                    filterPrice(cardsFilterByBrand)
+
+            if (checkedPrices.length > 0) {
+                const priceRange = checkedPrices[0].value;
+                if (priceRange == 1) {
+                    priceMatch = productPrice <= 100000;
+                } else if (priceRange == 2) {
+                    priceMatch = productPrice > 100000 && productPrice <= 200000;
+                } else if (priceRange == 3) {
+                    priceMatch = productPrice > 200000;
                 }
-            }        
-            
-        })
-     
-    }
-
-    
-
-// capturar el evento de captura cambios en los checkbox de price
-
-let newArrayFilterPrice
-let arrayFilterPrice, arrayNoFilterPrice
-for(i = 0; i < checkboxPrice.length; i++) {
-
-    checkboxPrice[i].addEventListener('change', function() {
-        console.log('Entro al filter')
-        let arr3 = []
-        let arr4 = [] 
-        for(i = 0; i < checkboxPrice.length; i++) {
-        if (checkboxPrice[i].checked) {            
-            arr3.push(checkboxPrice[i].value)
-            arrayFilterPrice = Array.from(new Set(arr3))            
-        } else {
-            arr4.push(checkboxPrice[i].value)
-            arrayNoFilterPrice = Array.from(new Set(arr4))
-            // el 14 corresponde al numero de filtros seteados. De agregar o reducir la cantidad, se debe modificar este numero
-            if (arrayNoFilterPrice.length == 3) {
-                arrayFilterPrice = []   
-            }   
-        }
-        } 
-
-            newArrayFilterPrice = arrayFilterPrice.join()
-            
-           
-       
-            if (cardsFilterByBrand.length != 0) {
-                console.log(cardsFilterByBrand)
-                filterPrice(cardsFilterByBrand)
-            } else {
-                console.log('chau')
-                filterPrice(cards)
-            } 
-         
-    })
- 
-}
-
-// capturar el evento de captura cambios en los checkbox de discount
-
-// let newArrayFilterDiscount
-// let arrayFilterDiscount, arrayNoFilterDiscount
-// for(i = 0; i < checkboxDiscount.length; i++) {
-
-//     checkboxDiscount[i].addEventListener('change', function() {
-//         let arr3 = []
-//         let arr4 = [] 
-//         for(i = 0; i < checkboxDiscount.length; i++) {
-//         if (checkboxDiscount[i].checked) {            
-//             arr3.push(checkboxDiscount[i].value)
-//             arrayFilterDiscount = Array.from(new Set(arr3))            
-//         } else {
-//             arr4.push(checkboxDiscount[i].value)
-//             arrayNoFilterDiscount = Array.from(new Set(arr4))
-//             // el 14 corresponde al numero de filtros seteados. De agregar o reducir la cantidad, se debe modificar este numero
-//             if (arrayNoFilterDiscount.length == 5) {
-//                 arrayFilterDiscount = []   
-//             }   
-//         }
-//         } 
-
-//             newArrayFilterDiscount = arrayFilterDiscount.join();
-          
-//             if (cardsFilterByBrand.length != 0) {
-//                 filterDiscount(cardsFilterByBrand)
-//             } else {
-//                 filterDiscount(cards)
-//             } 
-         
-//     })
- 
-// }
-
-
-    const filterBrand = () => {
-        
-        for(i = 0; i < cards.length; i++) {
-            if (arrayFilter.length != 0) {
-            // tengo que ver que si id-brand esta contenido en arrayFilter arrayFilter.indexOf(cards[i].dataset.brand) !== 0 . Si esta contenido entonces cards[i].style.display ="none"
-            if (arrayFilter.indexOf(cards[i].dataset.category) != -1){
-                cardsFilterByBrand.push(cards[i])              
-                cards[i].style.display = "block"
-            } else {
-                cards[i].style.display = "none"
             }
-        } else {
-            cards[i].style.display = "block"
-        }
-    }
-        return cardsFilterByBrand
-    }
 
-
-// Funcion para filtrar precios
-    const filterPrice = (arrayCards) => {
-       console.log()
-        for(i = 0; i < arrayCards.length; i++) {
-            if (arrayFilterPrice.length != 0) {                              
-                // revisar condiciones y precios
-                
-                switch (newArrayFilterPrice) {
-                    case "1":
-                    arrayCards[i].dataset.price < 100000 ? arrayCards[i].style.display = "block" : arrayCards[i].style.display = "none";
-                    break;
-                    case "2":
-                       100000 < arrayCards[i].dataset.price && arrayCards[i].dataset.price < 200000 ? arrayCards[i].style.display = "block" : arrayCards[i].style.display = "none";
-                       break;
-                    case "3":
-                        arrayCards[i].dataset.price >= 200000 ? arrayCards[i].style.display = "block" : arrayCards[i].style.display = "none";
-                    break;
-                  }
-        }
-    }
-    }
-
-// Funcion para filtrar descuento
-    // const filterDiscount = (arrayCards) => {
-    //     for(i = 0; i < arrayCards.length; i++) {
-    //         if (arrayFilterDiscount.length != 0) {                              
-    //             // revisar condiciones y precios
-                
-    //             switch (newArrayFilterDiscount) {
-    //                 case "1":
-    //                 arrayCards[i].dataset.discount >= 10 ? arrayCards[i].style.display = "block" : arrayCards[i].style.display = "none";
-    //                 break;
-    //                 case "2":
-    //                    arrayCards[i].dataset.discount >= 20 ? arrayCards[i].style.display = "block" : arrayCards[i].style.display = "none";
-    //                    break;
-    //                 case "3":
-    //                     arrayCards[i].dataset.discount >= 30 ? arrayCards[i].style.display = "block" : arrayCards[i].style.display = "none";
-    //                 break;
-    //                 case "4":
-    //                     arrayCards[i].dataset.discount >= 40 ? arrayCards[i].style.display = "block" : arrayCards[i].style.display = "none";
-    //                 break;
-    //                 case "5":
-    //                     arrayCards[i].dataset.discount >= 50 ? arrayCards[i].style.display = "block" : arrayCards[i].style.display = "none";
-    //                 break;
-    //               }
-    //     }
-    // }
-    // }
-
-
-    // button to clean ckecked brand
-
-    var buttonToClean = document.querySelector(".clean.brand") 
-
-    buttonToClean.addEventListener('click', () => {
-        for(var i=0;i<checkbox.length;i++) {
-            checkbox[i].checked = false;
-            console.log(arrayFilter)
-            arrayFilter = []
-            cardsFilterByBrand = []
-            filterBrand()
-            
-            if (newArrayFilterPrice) {
-                
-                console.log(newArrayFilterPrice)
-                filterPrice(cards)
+            if (brandMatch && categoryMatch && priceMatch) {
+                product.style.display = 'block';
+            } else {
+                product.style.display = 'none';
             }
-        }
-        
-    })
+        });
+    }
 
-    // button to clean ckecked price
+    // Add event listeners to checkboxes and radio buttons
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', filterProducts);
+    });
 
-    var buttonToCleanPrice = document.querySelector(".clean.price") 
+    priceRadios.forEach(radio => {
+        radio.addEventListener('change', filterProducts);
+    });
 
-    buttonToCleanPrice.addEventListener('click', () => {
-      
-        if (!arrayFilter) {
-        arrayFilter = []
-       }  
+    // Clean filters
+    document.querySelector('.clean.brand').addEventListener('click', () => {
+        checkboxes.forEach(checkbox => {
+            checkbox.checked = false;
+        });
+        filterProducts();
+    });
 
-        for(var i=0;i<checkboxPrice.length;i++) {
-        checkboxPrice[i].checked = false;
-        arrayFilterPrice = []
-        filterPrice(cards)
-        console.log(arrayFilter)
-       if (arrayFilter) {
-           console.log('entre')
-        filterBrand()
-       }
-        }
-        
-    })
-
-
-
-   
-
-} )
+    document.querySelector('.clean.price').addEventListener('click', () => {
+        priceRadios.forEach(radio => {
+            radio.checked = false;
+        });
+        filterProducts();
+    });
+});
